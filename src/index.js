97170e4759138fe178cdc900e3b6bf0baa4214ad
Eric Lee <szl0094@auburn.edu>
2022-04-27 15:28:17 -0500

    ES6 Class syntax to function syntax

--- src/index.js
+++ src/index.js
@@ -1,58 +1,55 @@
-class Square extends React.Component {
-  render() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square () {
     return (
-      <button className="square">
+        <button className="square">
         {/* TODO */}
-      </button>
-    );
-  }
+        </button>
+    )
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
+function Board () {
+    function renderSquare(i) {
+        return <Square />;
+    }
     const status = 'Next player: X';
 
     return (
-      <div>
+        <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
         </div>
-      </div>
+        </div>
     );
-  }
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+function Game () {
+    return  (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
     );
-  }
 }
 
 // ========================================
